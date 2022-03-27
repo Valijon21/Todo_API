450f5c6eaf089c11071ba7b1af4cde76ec0e5a55
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-03-27 21:24:49 +0500

    add views class

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from msilib.schema import ServiceInstall
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Toodo
+from .serializers import ToodoSerializer
 
 # Create your views here.
+#Toodo obyektlarni list kurinishida chiqarib beradi
+class ListToodo(ListAPIView):
+    queryset = Toodo.objects.all()
+    serailizer_class = ToodoSerializer
+
+# Bitta modelni uzini detailni yani bittasin kursatadi
+class DetailToodo(RetrieveAPIView):
+    queryset = Toodo.objects.all()
+    serializer_class = ToodoSerializer
